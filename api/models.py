b6035ac594c4ac05749a24c787d3c9a69980cf8c
Shogo1811 <shogo.n.1118@gmail.com>
2023-02-25 17:31:56 +0900

    Adminダッシュボード

--- api/models.py
+++ api/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
+
 def upload_avatar_path(instance, filename):
     ext = filename.split('.')[-1]
     return '/'.join(['avatars', str(instance.userProfile.id)+str(instance.nickName)+str(".")+str(ext)])
 
+
 def upload_post_path(instance, filename):
-  ext = filename.split('.')[-1]
-  return '/'.join(['posts', str(instance.userPost.id)+str(instance.title)+str(".")+str(ext)])
+    ext = filename.split('.')[-1]
+    return '/'.join(['posts', str(instance.userPost.id)+str(instance.title)+str(".")+str(ext)])
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
@@ -24,9 +27,10 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using= self._db)
+        user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -41,6 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
 class Profile(models.Model):
     nickName = models.CharField(max_length=20)
     userProfile = models.OneToOneField(
@@ -48,10 +53,12 @@ class Profile(models.Model):
         on_delete=models.CASCADE
     )
     created_on = models.DateTimeField(auto_now_add=True)
-    img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)
+    img = models.ImageField(blank=True, null=True,
+                            upload_to=upload_avatar_path)
 
     def __str__(self):
         return self.nickName
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -61,21 +68,12 @@ class Post(models.Model):
     )
     created_on = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(blank=True, null=True, upload_to=upload_post_path)
-    liked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked',blank=True)
+    liked = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='liked', blank=True)
 
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    text = models.CharField(max_length=100)
-    userComment = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name='userComment',
-        on_delete=models.CASCADE
-    )
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.text
 
 class Comment(models.Model):
     text = models.CharField(max_length=100)
